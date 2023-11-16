b85f23d698119afb28ecb4e079201abe99203239
anushkaagarwal15 <raanushka15@gmail.com>
2023-11-17 02:35:57 +0530

    New commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,40 +1,40 @@
-import React, {useState} from 'react' 
+import React, { useState } from 'react' 
+import { Link } from 'react-router-dom'
 import './Navbar.css'
 
 import logo from '../Assets/logo.png'
 import cart_icon from '../Assets/cart_icon.png'
 
 
-    // const: It's a keyword in JavaScript used to declare a variable that cannot be reassigned.
-
-    // menu: This is the name of the variable that will store the state value.
-    
-    // setMenu: This is the function that will be used to update the value of the menu state.
-    
-    // useState("shop"): This is a React Hook called useState. It is used in functional components to add state to them. The argument passed ("shop") is the initial value of the state variable (menu in this case). The initial value is set to "shop".
-    
-    // So, in simpler terms, this line of code is creating a state variable called menu that starts with the value "shop". The setMenu function can be used later to change the value of menu when needed, triggering a re-render of the component. This is a fundamental concept in React for managing and updating component state.
+   
 
     const Navbar = () => {
-const [menu, setMenu] = useState("shop");
+const [menu,setMenu] = useState("shop");
 
   return (
     <div className='navbar'>
       <div className='nav-logo'>
         <img src={logo} alt="" />
-        <p>Shopper</p>
+        <p>BOOKIFY</p>
         
       </div>
 
       <ul className="nav-menu">
-        <li onClick={() =>{setMenu("shop")}}>Shop{menu==="shop"?<hr/>:<></>}</li>
-        <li onClick={() =>{setMenu("men")}}>Men{menu==="men"?<hr/>:<></>}</li>
-        <li onClick={() =>{setMenu("women")}}>Women{menu==="women"?<hr/>:<></>}</li>
-        <li onClick={() =>{setMenu("kids")}}>Kids{menu==="kids"?<hr/>:<></>}</li>
+        <li onClick={() =>{setMenu("Shop")}}><Link to='/' style={{textDecoration : 'none'}}>Shop</Link>{menu==="Shop"?<hr/>:<></>}</li>
+
+        <li onClick={() =>{setMenu("men")}}><Link to='/men' style={{textDecoration : 'none'}}>Men</Link>{menu==="men"?<hr/>:<></>}</li>
+
+        <li onClick={() =>{setMenu("women")}}><Link to='/women' style={{textDecoration : 'none'}}>Women</Link>{menu==="women"?<hr/>:<></>}</li>
+
+        <li onClick={() =>{setMenu("kids")}}><Link to='/kids' style={{textDecoration : 'none'}}>Kids</Link>{menu==="kids"?<hr/>:<></>}</li>
+
+
       </ul>
       <div className="nav-login-cart">
-<button>Login</button>
-<img src={cart_icon} alt="" />
+     <Link to='/login'><button>Login</button></Link> 
+
+<Link to='/cart'><img src={cart_icon} alt="" /></Link>
+
 <div className="nav-cart-count">0</div>
       </div>
     </div>
---
+++ src/Components/Popular/Popular.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Popular.css'
+const Popular = () => {
+  return (
+    <div className="popular">
+      
+    </div>
+  )
+}
+
+export default Popular
